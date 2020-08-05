2560f89f530fa523f18760940041a2bd1227a2f6
Filipe Câncio <filipe.cancio@outlook.com>
2020-08-05 01:19:58 -0300

    update project

---
+++ nlw2/server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+app.post('/users',(request,response)=>{
+    console.log(request.body)
+    const users = [
+        {name:'Diego',age:25},
+        {name:'Vini',age:21},
+    ];
+    return response.json(users)
+});
+
+app.listen(3333);
